f51716edd35542eebf66416c19dbc1719332d8f6
Aidan Feldman <aidan.feldman@gmail.com>
2014-08-07 04:23:56 -0400

    make single file for tests

--- README.md
+++ README.md
@@ -2,16 +2,6 @@
 
 To most people building websites, writing JavaScript means copying-and-pasting a couple of jQuery snippets onto a page to get a button to animate.  While the proliferation of JavaScript frameworks has helped make code more organized, testing front-end code is still far from the norm.  We will go through the different types of testing, how to set up a test runner, writing unit and integration tests, and refactoring code to make it test*able*.  You will leave this talk fitter, happier, and less likely to create globals.
 
-## Outline
-
-* Basics of testing
-  * Write a test framework
-* Demos
-  * Stubbing/mocking
-    * [Sinon.JS](http://sinonjs.org)
-  * Testing w/ DOM interaction
-  * Global state
-
 ## See also
 
 * [JavaScript test framework comparison](https://coderwall.com/p/ntbixw)
---
+++ jasmine/tests.js
@@ -0,0 +1,88 @@
+var count = 3;
+function decrement(){
+  count -= 1;
+}
+
+
+describe('decrement()', function(){
+  it("reduces the count by one", function(){
+    decrement();
+    expect(count).toBe(2);
+  });
+
+  it("stops when it gets to zero");
+});
+
+
+
+
+
+
+
+
+
+
+
+function getDayName(){
+  var days = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  };
+
+  var now = new Date();
+  var dayNum = now.getDay();
+  return days[dayNum];
+}
+
+xdescribe('getDayName()', function(){
+  it("returns the day as a string", function(){
+    expect(getDayName()).toBe('Thursday');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+var LoadingMessage = function(){
+  this.$el = $('#loading');
+};
+
+LoadingMessage.prototype.show = function(){
+  this.$el.show();
+};
+
+LoadingMessage.prototype.hide = function(){
+  this.$el.hide();
+};
+
+var message = new LoadingMessage();
+
+
+xdescribe('LoadingMessage', function(){
+  describe('.hide()', function(){
+    it("makes the element invisible", function(){
+      message.hide();
+      expect($('#loading:visible').length).toBe(0);
+    });
+  });
+
+  describe('.show()', function(){
+    xit("makes the element visible", function(){
+      message.show();
+      expect($('#loading:visible').length).toBe(1);
+    });
+  });
+})
